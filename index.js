7d42495b03150892ce239e48ba72a6a8ddb92fa9
FATI <96890788+fatidevelopmentteam@users.noreply.github.com>
2023-02-08 16:54:19 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+//-----
 const WebSocket = require("ws");
 const { writeFile } = require("./DBTools");
 const database = require("./history.json");
@@ -6,12 +12,12 @@ let lastToday = null;
 const addToDay = (ToDay = lastToday) => {
   lastToday = ToDay;
   let newDB = database,
-  todayIndex = database.findIndex((e)=> e.id == ToDay);
+    todayIndex = database.findIndex((e) => e.id == ToDay);
   if (database[todayIndex]) {
     newDB[todayIndex].end = Date.now();
     writeFile(databaseURL, newDB);
   } else {
-    newDB.push({id:ToDay, start: Date.now(), end: null });
+    newDB.push({ id: ToDay, start: Date.now(), end: null });
     writeFile(databaseURL, newDB);
   }
 };
@@ -19,10 +25,10 @@ const addToDay = (ToDay = lastToday) => {
 const wsServer = new WebSocket.Server({ port: 2993 });
 wsServer.on("connection", (wsClient) => {
   wsClient.on("message", (data) => {
-    if (data.toString() == 'gethistory') {
-        wsClient.send(JSON.stringify(database))
-    }else {
-        addToDay(JSON.parse(data.toString()).now)
+    if (data.toString() == "gethistory") {
+      wsClient.send(JSON.stringify(database));
+    } else {
+      addToDay(JSON.parse(data.toString()).now);
     }
   });
   wsClient.on("close", (event) => {
@@ -30,5 +36,15 @@ wsServer.on("connection", (wsClient) => {
   });
 });
 
+app.get("/api/gethistory", cors(), (req, res) => {
+  res.send(database);
+});
+app.get("/api/getcurrentsession", cors(), (req, res) => {
+  res.send(database[database.findIndex((e) => e.id == lastToday)]);
+});
+
+app.listen(port, () => {
+  console.log(`Web API is running in port:${port}.`);
+});
 
 module.exports = {};
